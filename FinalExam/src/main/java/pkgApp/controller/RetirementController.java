ba2493d2c62cb1d861a1d042c28f2b54d8b3b279
Christopher <Christopher@WINDOWS-L4M9IUT>
2017-12-11 22:08:08 -0500

    Sorry about that

--- FinalExam/src/main/java/pkgApp/controller/RetirementController.java
+++ FinalExam/src/main/java/pkgApp/controller/RetirementController.java
@@ -1,13 +1,16 @@
 package pkgApp.controller;
 
 import java.net.URL;
+import java.text.NumberFormat;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import pkgApp.RetirementApp;
+import pkgCore.Retirement;
 
 public class RetirementController implements Initializable {
 
@@ -16,6 +19,20 @@ public class RetirementController implements Initializable {
 	
 	@FXML
 	private TextField txtYearsToWork;
+	@FXML
+	private TextField txtAnnualReturnWork;
+	@FXML
+	private TextField txtYearsRetired;
+	@FXML
+	private TextField txtAnnualReturnRetired;
+	@FXML
+	private TextField txtRequiredIncome;
+	@FXML
+	private TextField txtMonthlySSI;
+	@FXML
+	private TextField txtSaveEachMonth;
+	@FXML
+	private TextField txtNeedToSave;
 	
 
 	public RetirementApp getMainApp() {
@@ -34,14 +51,142 @@ public class RetirementController implements Initializable {
 	public void btnClear(ActionEvent event) {
 		System.out.println("Clear pressed");
 		
-		//	TODO: Clear all the text inputs
+		// Clear all the text inputs
+		
+		txtYearsToWork.clear();
+		txtAnnualReturnWork.clear();
+		txtYearsRetired.clear();
+		txtAnnualReturnRetired.clear();
+		txtRequiredIncome.clear();
+		txtMonthlySSI.clear();
+		
 	}
+
 	
 	@FXML
 	public void btnCalculate(ActionEvent event) {
 		
-		//	TODO: Call AmountToSave and TotalAmountSaved and populate 
+		// Call AmountToSave and TotalAmountSaved and populate 
 		
-	}
+		if (!ValidInput()) {
+			return;
+		}
+		Retirement retirement = new Retirement(Integer.parseInt(txtYearsToWork.getText()), 
+				Double.parseDouble(txtAnnualReturnWork.getText()),
+				Integer.parseInt(txtYearsRetired.getText()), 
+				Double.parseDouble(txtAnnualReturnRetired.getText()),
+				Double.parseDouble(txtRequiredIncome.getText()),
+				Double.parseDouble(txtMonthlySSI.getText()));
+		
+		txtNeedToSave.setText("$ "+String.format("%.2f",retirement.TotalAmountSaved()));
+		txtSaveEachMonth.setText("$ "+String.format("%.2f",retirement.AmountToSave()));
+}
+
+		private boolean ValidInput(){
+			if (txtAnnualReturnWork.getText().isEmpty()) {
+				 Alert _alert = new Alert(Alert.AlertType.ERROR);
+				 _alert.setContentText("Invalid can not be empty");
+				 _alert.show();
+				 return false;
+			}
+			try {
+				double workReturn = Double.parseDouble(txtAnnualReturnWork.getText());
+				if (workReturn<0||workReturn>0.2) {
+					Alert _alert = new Alert(Alert.AlertType.ERROR);
+					 _alert.setContentText("Invalid number must be between 0 and 0.2");
+					 _alert.show();
+					 return false;
+				}
+			} catch (NumberFormatException e) {
+				Alert _alert = new Alert(Alert.AlertType.ERROR);
+				 _alert.setContentText("Invalid number");
+				 _alert.show();
+				 return false;
+			}
+			
+			if (txtYearsToWork.getText().isEmpty()) {
+				 Alert _alert = new Alert(Alert.AlertType.ERROR);
+				 _alert.setContentText("Invalid number");
+				 _alert.show();
+				 return false;
+			}
+			try {
+				Integer.parseInt(txtYearsToWork.getText());
+			} catch (NumberFormatException e) {
+				Alert _alert = new Alert(Alert.AlertType.ERROR);
+				 _alert.setContentText("Invalid number");
+				 _alert.show();
+				 return false;
+			}
+			
+			if (txtYearsRetired.getText().isEmpty()) {
+				 Alert _alert = new Alert(Alert.AlertType.ERROR);
+				 _alert.setContentText("Invalid can not be empty!");
+				 _alert.show();
+				 return false;
+			}
+			try {
+				Integer.parseInt(txtYearsRetired.getText());
+			} catch (NumberFormatException e) {
+				Alert _alert = new Alert(Alert.AlertType.ERROR);
+				 _alert.setContentText("Invalid number");
+				 _alert.show();
+				 return false;
+			}
+			
+			if (txtAnnualReturnRetired.getText().isEmpty()) {
+				 Alert _alert = new Alert(Alert.AlertType.ERROR);
+				 _alert.setContentText("invalid can not be empty");
+				 _alert.show();
+				 return false;
+			}
+			try {
+				double retireReturn = Double.parseDouble(txtAnnualReturnRetired.getText());
+				if (retireReturn<0||retireReturn>0.03) {
+					Alert _alert = new Alert(Alert.AlertType.ERROR);
+					 _alert.setContentText("Invalid number must be between 0 and 0.03");
+					 _alert.show();
+					 return false;
+				}
+			} catch (NumberFormatException e) {
+				Alert _alert = new Alert(Alert.AlertType.ERROR);
+				 _alert.setContentText("Invalid number");
+				 _alert.show();
+				 return false;
+			}
+			
+			if (txtRequiredIncome.getText().isEmpty()) {
+				 Alert _alert = new Alert(Alert.AlertType.ERROR);
+				 _alert.setContentText("Invalid can not be empty");
+				 _alert.show();
+				 return false;
+			}
+			try {
+				Double.parseDouble(txtRequiredIncome.getText());
+			} catch (NumberFormatException e) {
+				Alert _alert = new Alert(Alert.AlertType.ERROR);
+				 _alert.setContentText("invalid number");
+				 _alert.show();
+				 return false;
+			}
+			
+			if (txtMonthlySSI.getText().isEmpty()) {
+				 Alert _alert = new Alert(Alert.AlertType.ERROR);
+				 _alert.setContentText("Invalid can not be empty");
+				 _alert.show();
+				 return false;
+			}
+			try {
+				Double.parseDouble(txtMonthlySSI.getText());
+			} catch (NumberFormatException e) {
+				Alert _alert = new Alert(Alert.AlertType.ERROR);
+				 _alert.setContentText("invalid number");
+				 _alert.show();
+				 return false;
+			}
+			
+			return true;
+		}
+		
 	
 }
--- FinalExam/src/main/java/pkgCore/Retirement.java
+++ FinalExam/src/main/java/pkgCore/Retirement.java
@@ -9,22 +9,100 @@ public class Retirement {
 	private double dRequiredIncome;
 	private double dMonthlySSI;
 	
-	//TODO: Build the contructor, getters and setters for the attributes above.
 	
 	public double AmountToSave()
 	{
-		//TODO: Determine the amount to save each month based on TotalAmountSaved, YearsToWork
-		//		and Annual return while working
+		// Determine the amount to save each month based on TotalAmountSaved, YearsToWork
+		// and Annual return while working
 		
-		
-		return 0;
+		double rAnnualReturnWorking = dAnnualReturnWorking / 12;
+		double dMonthsToWork = iYearsToWork * 12;
+		double returnVal = 0;
+		if (rAnnualReturnWorking == 0) {
+			returnVal = TotalAmountSaved()/dMonthsToWork;
+	    }else {
+	    	returnVal = TotalAmountSaved()* rAnnualReturnWorking
+	                  /(Math.pow(1+rAnnualReturnWorking, dMonthsToWork)-1);
+	    }
+		return returnVal;
+	
+	}
+	
+	public Retirement(int iYearsToWork, double dAnnualReturnWorking, int iYearsRetired, double dAnnualReturnRetired,
+			double dRequiredIncome, double dMonthlySSI) {
+		super();
+		this.iYearsToWork = iYearsToWork;
+		this.dAnnualReturnWorking = dAnnualReturnWorking;
+		this.iYearsRetired = iYearsRetired;
+		this.dAnnualReturnRetired = dAnnualReturnRetired;
+		this.dRequiredIncome = dRequiredIncome;
+		this.dMonthlySSI = dMonthlySSI;
+	}
+	
+	// Build the constructor, getters and setters for the attributes above.
+	
+	public int getiYearsToWork() {
+		return iYearsToWork;
+	}
+
+	public void setiYearsToWork(int iYearsToWork) {
+		this.iYearsToWork = iYearsToWork;
+	}
+
+	public double getdAnnualReturnWorking() {
+		return dAnnualReturnWorking;
+	}
+
+	public void setdAnnualReturnWorking(double dAnnualReturnWorking) {
+		this.dAnnualReturnWorking = dAnnualReturnWorking;
+	}
+
+	public int getiYearsRetired() {
+		return iYearsRetired;
+	}
+
+	public void setiYearsRetired(int iYearsRetired) {
+		this.iYearsRetired = iYearsRetired;
+	}
+
+	public double getdAnnualReturnRetired() {
+		return dAnnualReturnRetired;
+	}
+
+	public void setdAnnualReturnRetired(double dAnnualReturnRetired) {
+		this.dAnnualReturnRetired = dAnnualReturnRetired;
+	}
+
+	public double getdRequiredIncome() {
+		return dRequiredIncome;
+	}
+
+	public void setdRequiredIncome(double dRequiredIncome) {
+		this.dRequiredIncome = dRequiredIncome;
+	}
+
+	public double getdMonthlySSI() {
+		return dMonthlySSI;
+	}
+
+	public void setdMonthlySSI(double dMonthlySSI) {
+		this.dMonthlySSI = dMonthlySSI;
 	}
 	
 	public double TotalAmountSaved()
 	{
-		//	TODO: Determine amount to be saved based on Monthly SSI, Required Income, Annual return during retirement
-		//		and number of years retired.
+		// Determine amount to be saved based on Monthly SSI, Required Income, Annual return during retirement
+		// and number of years retired.
 		//
-		return 0;
+		
+		double dARR = dAnnualReturnRetired / 12;
+		double ReturnVal = 0;
+		int dYR = iYearsRetired*12;
+		if (dARR == 0) {
+			ReturnVal = TotalAmountSaved() / dARR;
+		} else {
+			ReturnVal = TotalAmountSaved() * dARR / (Math.pow(1 + dARR, dARR) - 1);
+		}
+		return ReturnVal;
 	}
 }
